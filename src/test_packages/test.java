c4bf812ab6ddca395b8be40a140ca4b4b8cfe991
Louis <louis17b@gmail.com>
2022-12-14 00:27:42 +0100

    correction packages test

--- src/test_packages/test.java
+++ src/test_packages/test.java
@@ -1,16 +1,10 @@
 package test_packages;
 
 import modele.Joueurs;
-import modele.Joueurs.Filiere;
 import modele.Zone;
-import modele.Zone.nomZone;
 
 public class test {
 	public static void main(String[] args) {
-			
-			Joueurs j = new Joueurs("test",Filiere.ISI);
-			Zone z = new Zone(nomZone.Bibliotheque);
-			
-			j.choisirReserviste();
+
 	}
 }
